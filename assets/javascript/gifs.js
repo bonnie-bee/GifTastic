53fbd1cde0e8d256ed903c045052a2cd3c95e743
notsilverman <birchba@gmail.com>
2018-06-09 00:07:42 -0500

    The gifs show up and start on click

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,14 +1,14 @@
 //Array of topics to turn into buttons
-const topics = ["Clueless", "Girls Trip", "Crazy Rich Asians", "When Harry Met Sally", "Moana", "Bridget Jones Diary", "Bridesmaids", "Moonstruck",  "Bend It Like Beckham", "To Wong Foo With Love From Julie Newmar"];
+const topics = ["Clueless", "Girls Trip", "Crazy Rich Asians", "When Harry Met Sally", "Moana", "Bridget Jones Diary", "Bridesmaids", "Moonstruck", "Bend It Like Beckham", "To Wong Foo With Love From Julie Newmar"];
 
 //Make buttons
-for(let i=0; i<topics.length; i++){
+for (let i = 0; i < topics.length; i++) {
     let buttons = $('<button>')
-    .addClass('movies')
-    .attr('data-type', topics[i])
-    .text(topics[i]);
+        .addClass('movies')
+        .attr('data-type', topics[i])
+        .text(topics[i]);
 
-//Append buttons to .buttonSpace
+    //Append buttons to .buttonSpace
     $('.buttonSpace').append(buttons)
 }
 
@@ -16,11 +16,83 @@ for(let i=0; i<topics.length; i++){
 
 
 //Click button to get static gifs
-function getGifs(){
-    let queryURL = 
-}
-//Ajax query
+$('.movies').on('click', function () {
+    const name = $(this).attr('data-type');
+    // const key = "cqSSpY4XuKE5YrQJnI8qVFRtFBMTJvg4";
+    const queryURL = `http://api.giphy.com/v1/gifs/search?q=${name}&api_key=cqSSpY4XuKE5YrQJnI8qVFRtFBMTJvg4&limit=10`
+
+
+    //empty the gif space each time you click a button
+    $('.gifSpace').empty()
+
+    //Ajax query
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        let stillUrl;
+        let movingUrl;
+
+
+        for (let i = 0; i < response.data.length; i++) {
+
+
+            stillUrl = response.data[i].images.fixed_height_still.url;
+            movingUrl = response.data[i].images.fixed_height.url;
+
+            let movieImg = $("<img>")
+                .addClass('gifImg')
+                .attr('src', stillUrl)
+                .attr('alt', 'gif of a movie scene')
+                .attr('data-state', 'still')
+                .attr('animater', movingUrl)
+
+            $('.gifSpace').prepend(movieImg);
+
+
+            startGif();
+
+
+        }
+
+        
+    })
+
+});
+
+
 //Turn gifs on
+
+function startGif() {
+    $('.gifImg').on('click', function () {
+
+
+        if ($(this).attr('data-state') === 'still') {
+            
+            $(this).attr('data-state', 'animate')
+            
+            let a = $(this).attr('animater')
+            $(this).attr('src', a)
+        } 
+
+        console.log('this ' + $(this).attr('data-state'))
+        console.log('source ' + $(this).attr('src'))
+
+    });
+
+}
+
+
+
+
+
+
+
+    
+
+
 //Turn gifs off
 //Show rating below each gif
 
